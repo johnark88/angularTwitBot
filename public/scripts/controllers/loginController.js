557b7fca3110636acef036c57bbaf387561980b6
John Arkema <john.arkema@gmail.com>
2017-01-13 15:40:53 -0600

    login page

--- public/scripts/controllers/loginController.js
+++ public/scripts/controllers/loginController.js
@@ -1,3 +1,6 @@
 myApp.controller('loginController', ['$scope', '$http', function($scope, $http) {
   console.log('Log in Controller');
+  $scope.logIn = function() {
+    console.log('logging in');
+  };
 }]);
