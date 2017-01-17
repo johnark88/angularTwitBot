929569758046b5250df4857e42e188e48cb41f63
John Arkema <john.arkema@gmail.com>
2017-01-17 15:58:42 -0600

    firebase re-tool

--- server/app.js
+++ server/app.js
@@ -1,37 +1,33 @@
 var dotenv = require('dotenv');
-var firebase = require("firebase");
 var express = require('express');
 var app = express();
-var path = require('path');
 var bodyParser = require('body-parser');
 var path = require('path');
-var logger = require('morgan');
-var cookieParser = require('cookie-parser');
-var session = require('express-session');
 var urlencodedParser = bodyParser.urlencoded({
     extended: false
 });
 var decoder = require('./routes/decoder');
 var login = require('./routes/login');
 
+//this route serves the index
 
+app.get('/', function(req, res){
+  res.sendFile(path.resolve('./public/index.html'));
+});
+//use public folder
+app.use(express.static('public'));
+
+app.use(decoder.token);
 
 app.use(bodyParser.json());
-app.use(logger('dev'));
-app.use(cookieParser());
+app.use("/login", login);
 
 // Imports all .env values
 dotenv.load();
 
 
-//this route serves the index
-var index = require('./routes/index');
-
 //this route serves the twitter bot
 // var bot = require('./routes/bot');
-
-
-app.use("/login", login);
 
 
 // display the error and stacktrace accordingly.
@@ -43,6 +39,3 @@ app.use(function(err, req, res, next) {
 app.listen(process.env.PORT, function() {
     console.log('I am listening on ', process.env.PORT);
 });
-
-//use public folder
-app.use(express.static('public'));
