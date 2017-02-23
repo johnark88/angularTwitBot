0e3a2cc3e8176363f9d2819185a5e4070c3eb5cf
John Arkema <john.arkema@gmail.com>
2017-02-23 17:34:32 -0600

    still 403

--- server/app.js
+++ server/app.js
@@ -1,44 +1,32 @@
 var dotenv = require('dotenv');
 var express = require('express');
 var app = express();
+var path = require('path');
 var bodyParser = require('body-parser');
 var urlencodedParser = bodyParser.urlencoded({extended: false});
-var path = require('path');
-var morgan = require('morgan');
-
+//decode token
+var decoder = require('./routes/decoder');
 // Imports all .env values
 dotenv.load();
 var portDecision = process.env.PORT || 5000;
-
-//decode token
-var decoder = require('./routes/decoder');
-//verify decoded token
-var login = require('./routes/login');
-
-//these route serves the twitter bot
-var bot = require('./routes/bot');
-var favBot = require('./routes/favBot');
-var test = require('./routes/test');
 
 //this route serves the index
 app.get('/', function(req, res){
   res.sendFile(path.resolve('./public/index.html'));
 });
-
 //use public folder
 app.use(express.static('public'));
-
-//use this to decode token
+app.use(bodyParser.json());
 app.use(decoder.token);
 
-app.use(bodyParser.json());
-app.use(morgan('dev'));
-
+var login = require('./routes/login');
 app.use("/login", login);
+var bot = require('./routes/bot');
 app.use("/bot", bot);
+var favBot = require('./routes/favBot');
 app.use("/favBot", favBot);
+var test = require('./routes/test');
 app.use("/test", test);
-
 
 app.listen(portDecision, function() {
     console.log('I am listening on ', portDecision);
--- server/routes/test.js
+++ server/routes/test.js
@@ -7,16 +7,27 @@ var connectionString = 'postgres://localhost:5432/johnA';
 console.log('test');
 
 
-router.post('/', urlencodedParser, function(req,res){
-  console.log('post route');
-  console.log(req.body);
-  if (err) {
-    console.log(err)
-    res.sendStatus(500);
-  } else {
-  console.log('connected to DB');
-  res.send('testResults');
-}
+router.get("/", urlencodedParser, function(req, res){
+  pg.connect(connectionString, function(err, client, done){
+    var userEmail = req.decodedToken.email;
+    console.log(req.body.hash, 'reqqqqqqqq');
+    // Check the user's level of permision based on their email
+    client.query('SELECT clearance_level FROM users WHERE email=$1', [userEmail], function(err, clearanceLevelQueryResult){
+      done();
+      if(err){
+        console.log('Error COMPLETING clearance_level query task', err);
+        res.sendStatus(500);
+      }else{
+        pg.connect(connectionString, function(err, client, done){
+          if(clearanceLevelQueryResult.rowCount === 0) {
+            // If the user is not in the database, return a forbidden error status
+            console.log('No user found with that email. Have you added this person to the database? Email: ', req.decodedToken.email);
+            res.sendStatus(403);
+          }
+          done();
+        });
+      }
+    });
+  });
 });
-
 module.exports = router;
